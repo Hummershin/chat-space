15af960e2b109b7a0dc205cf1059c7c864031bed
Hummershin <64775577+Hummershin@users.noreply.github.com>
2020-06-16 22:28:31 +0900

    deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,11 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+working_directory "#{app_path}/current"
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/tmp/pids/unicorn.pid"
